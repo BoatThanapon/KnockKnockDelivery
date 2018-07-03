c87df68864527ff27b923545bf5c37049862091d
BoatThanapon <Thanapon_chap@hotmail.com>
2018-07-04 00:58:43 +0700

    Update admin page

---
+++ frontend/src/app/services/admin.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+
+  private baseUrl = 'http://localhost:8000/api/';
+
+  constructor(private http: HttpClient) { }
+
+  getAllUserInSystem(role_id) {
+    let body = {
+      "role_id": role_id,
+      "profile_status_user_id": 2
+    }
+    return this.searchUsers(body)
+
+  }
+
+  getAllHoldingUser(role_id) {
+    let body = {
+      "role_id": role_id,
+      "profile_status_user_id": 1
+    }
+    return this.searchUsers(body)
+  }
+
+  searchUsers(body) {
+    return  this.http.post<users>(`${this.baseUrl}search-user`, body)
+  }
+
+  updateUserStatus(role_id,uid) {
+    let body = {
+      "role_id": role_id,
+      "profile_status_user_id": 1
+    }
+    return  this.http.put(`${this.baseUrl}admin/updatestatus/profile/`+uid, body)
+
+  }
+
+}
+
+export interface users {
+  data: [
+    {
+        seller_name: null,
+        shop_name: null,
+        shop_location: null,
+        shop_type: {
+            shop_type_id: null,
+            shop_type_name: null
+        },
+        profile_status: {
+            profile_status_id: null,
+            profile_status_name: null
+        },
+        profile_id: null
+    }
+]
+}
