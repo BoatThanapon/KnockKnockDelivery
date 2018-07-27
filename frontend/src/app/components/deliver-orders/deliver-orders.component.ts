7d0a35996557ca7a9fcfdc2d06014bdaaf163d52
Buri <beeforce@hotmail.com>
2018-07-28 00:53:43 +0700

    service charge

--- frontend/src/app/components/deliver-orders/deliver-orders.component.ts
+++ frontend/src/app/components/deliver-orders/deliver-orders.component.ts
@@ -167,7 +167,16 @@ export class DeliverOrdersComponent implements OnInit {
         this.calculateDistance(+element.seller.shop_latitude,+element.seller.shop_longitude,
           +element.receiver_latitude,+element.receiver_longitude);
           element["distance"] = this.distance;
-          element["serviceCharge"] = this.distance;
+          if(this.distance > 8 && this.distance <= 40){
+          element["service_charge"] = Math.round(25+((this.distance-1)*14))
+          }else if (this.distance > 5 && this.distance <= 8){
+            element["service_charge"] = Math.round(25+((this.distance-1)*11))
+          }
+          else if (this.distance > 1 && this.distance <= 5){
+            element["service_charge"] = Math.round(25+((this.distance-1)*8))
+          }else{
+            element["service_charge"] = 25;
+          }
         })
 
 
